b0b64e029307b13847384ef0a9ef28fb2e289999
damasio <damasiodigo77@gmail.com>
2025-05-08 02:02:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  setTimeout(() => {
+    const feliz = document.createElement('div');
+    feliz.classList.add('feliz');
+    feliz.innerHTML = 'Fico tão feliz por estar com você! 💕';
+    document.body.appendChild(feliz);
+  }, 2000);
